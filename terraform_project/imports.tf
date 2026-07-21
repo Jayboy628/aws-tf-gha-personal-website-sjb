c4b72adc5d4642bc975b4c65d8eb0d2dd6c305df
jayboy628 <shaunjay.brown@gmail.com>
2026-07-21 19:56:34 -0400

    Rename S3 bucket resource using moved block

--- terraform_project/imports.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = aws_s3_bucket.statictic_site
-  id = var.bucket_name
-}
--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -1,8 +1,13 @@
-resource "aws_s3_bucket" "statictic_site" {
+moved {
+  from = aws_s3_bucket.statictic_site
+  to   = aws_s3_bucket.static_site
+}
+
+resource "aws_s3_bucket" "static_site" {
   bucket = var.bucket_name
 
   tags = {
-    Name        = "My bucket"
+    Name        = "Personal Website"
     Environment = "Dev"
   }
 }
