abdb56d7e022ba1487b61e45f5830c22040daaec
jayboy628 <shaunjay.brown@gmail.com>
2026-07-21 20:09:51 -0400

    Update action workflow: Add terraform

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -11,3 +11,53 @@ resource "aws_s3_bucket" "static_site" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "static_website_config" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+data "aws_iam_policy_document" "static_site_policy" {
+  statement {
+    sid    = "PublicReadWebsiteObjects"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.static_site.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+  policy = data.aws_iam_policy_document.static_site_policy.json
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.static_site
+  ]
+}
