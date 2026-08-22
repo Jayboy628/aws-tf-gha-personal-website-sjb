3721d8646ed5798a85a4a1ff7dc7147b0bb2316e
jayboy628 <shaunjay.brown@gmail.com>
2026-08-22 19:13:53 -0400

    fix S3 bucket output reference

--- infa/outputs.tf
+++ infa/outputs.tf
@@ -1,4 +1,4 @@
-# s3 bucket output
-output "s3_bucket" {
-  value = aws_s3_bucket.website_bucket.id
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket hosting the personal website."
+  value       = aws_s3_bucket.static_site.id
 }
