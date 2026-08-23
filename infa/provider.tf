f1e2cfdc317f1d2cd5c253fbe74871f493923d77
jayboy628 <shaunjay.brown@gmail.com>
2026-08-22 20:09:48 -0400

    update Terraform backend locking and deployment workflow

--- infa/provider.tf
+++ infa/provider.tf
@@ -1,18 +1,16 @@
-
 provider "aws" {
   region = "us-east-1"
-
-
 }
 
 terraform {
   backend "s3" {
-    bucket         = "tf-resources-gha-sjb"
-    key            = "github-actions/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "tf-resources-gha-sjb-lock"
+    bucket       = "tf-resources-gha-sjb"
+    key          = "github-actions/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
